7fa1f7cb7a587632bfbad8c9ffce3d97831b16f8
zhang <2665457825@qq.com>
2020-12-28 16:07:13 +0800

    这是第一次

---
+++ pages/index/index.js
@@ -0,0 +1,21 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+   
+  },
+  //事件处理函数
+  bindViewTap: function() {
+   
+  },
+  onLoad: function () {
+    
+  },
+  ontTap: function () {
+    wx.navigateTo({url: 'pages/logs/logs',
+    })
+},
+  
+})
